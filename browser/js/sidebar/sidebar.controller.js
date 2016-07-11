2a442cff3b9a74ff9149c08e4b671cc2f7def056
Alec Friedman <alec.friedman3@gmail.com>
2016-07-11 10:38:28 -0400

    Forms working

--- browser/js/sidebar/sidebar.controller.js
+++ browser/js/sidebar/sidebar.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-juke.controller('SidebarCtrl', function ($scope) {
+juke.controller('SidebarCtrl', function ($scope, $log) {
 
   // nothing to see here for now… state transitions happening with ui-sref!
 
